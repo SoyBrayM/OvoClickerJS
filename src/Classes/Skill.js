9f9a8d279368ff369f0b4da8a9fae2156ad5f5dc
Bray Marin <bray.marin@upb.edu.co>
2023-11-09 16:48:54 -0500

    Finished the Skill logic and stylizing

--- src/Classes/Skill.js
+++ src/Classes/Skill.js
@@ -1,37 +1,54 @@
 class Skill {
   constructor() {
-    this.level = 0;
-    this._duration = 0;
-    this._cooldown = 0;
+    this.level = 1;
+    this._duration;
+    this._cooldown;
+    this._multiplier;
   }
 
   Activate(player) {
-    if (this._cooldown > 0) {
+    console.log("Skill activated!");
+    if (this.level == 0) {
+      alert("Skill is level 0, upgrade it first to use it!");
+      return;
+    } else if (this._cooldown > 0) {
       alert("Skill is on cooldown!");
       return;
+    } else if (this._duration > 0) {
+      alert("Skill is already active!");
+      return;
+    } else {
+      this._multiplier =
+        Math.floor(Math.log(Math.floor((this.level + 4) / (Math.PI / 2)))) + 1;
+      this._duration =
+        (this._multiplier * this.level) /
+          (this._multiplier * this._multiplier) +
+        5;
+      document.getElementById("TapPowerDisplay").style.color = "#D81159";
+      player.TapPower *= this._multiplier;
+      let durationInterval = setInterval(() => {
+        this._duration -= 0.01;
+        document.getElementById("SkillButton").innerText = "Cooldown: " + this._cooldown.toFixed(2);
+        if (this._duration <= 0) {
+          clearInterval(durationInterval);
+        }
+      }, 10);
     }
-    
-    setTimeout(() => {},  )
   }
 
-  Deactivate() {
+  Deactivate(player) {
+    player.TapPower /= this._multiplier;
+    document.getElementById("TapPowerDisplay").style.color = "black";
+    this._cooldown = this._duration + 10;
     this._duration = 0;
-  }
 
-  get Duration() {
-    return this._duration;
-  }
-
-  set Duration(value) {
-    this._duration = value;
-  }
-
-  get Cooldown() {
-    return this._cooldown;
-  }
-
-  set Cooldown(value) {
-    this._cooldown = value;
+    let cooldownInterval = setInterval(() => {
+      this._cooldown -= 0.01;
+      document.getElementById("SkillButton").innerText = "Cooldown: " + this._cooldown.toFixed(2);
+      if (this._cooldown <= 0) {
+        clearInterval(cooldownInterval);
+      }
+    }, 10);
   }
 }
 

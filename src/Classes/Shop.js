8cfd5835df1dca2a43f633d7e64cebeaa5bff6d3
Bray Marin <bray.marin@upb.edu.co>
2023-11-09 16:52:34 -0500

    Implemented simple shop buying logic

--- src/Classes/Shop.js
+++ src/Classes/Shop.js
@@ -4,7 +4,23 @@ class Shop {
   }
 
   UpgradeTapPower (player) {
+    if (player.OvoPoints < 1) {
+      alert("You don't have enough Ovo Points!");
+      return;
+    }
+    player.OvoPoints--;
     player.TapPower++;
+  }
+  
+  UpgradeSkill (player, skill) {
+    if (player.OvoPoints < 1) {
+      alert("You don't have enough Ovo Points!");
+      return;
+    }
+    document.getElementById("SkillButton").innerText = "Skill Ready to Use!";
+    document.getElementById("SkillButton").style.backgroundColor = "#D81159";
+    player.OvoPoints--;
+    skill.level++;
   }
 }
 
